e06f7fe4cdcb185685287efae2aa6fc1a1f95a29
Taylor Hartley <twh0008@auburn.edu>
2016-05-04 01:15:17 -0500

    Fixed ArrayOutOfBounds exception in ViewGrades
    If the student had more courses than grades, the for statement would go
    out of bounds resulting in an exception. Put a try/catch statement to
    put "Not Entered" into the table instead of closing the program.

--- src/student/viewgrades/ViewGradesModel.java
+++ src/student/viewgrades/ViewGradesModel.java
@@ -23,7 +23,11 @@ public class ViewGradesModel {
 		for(int i = 0; i < courses.size(); i++){
 			tableInfo[i][0] = courses.get(i);
 			System.out.println(tableInfo[i][0]);
+			try {
 			tableInfo[i][1] = grades.get(i);
+			} catch (Exception e) {
+				tableInfo[i][1] = "Not Entered";
+			}
 			System.out.println(tableInfo[i][1]);
 		}
 		System.out.println("Finished");
